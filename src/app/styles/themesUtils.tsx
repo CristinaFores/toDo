bbf560859707c5096baca94ada085f8d234a8b77
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-10-31 22:41:00 +0100

    Customize component ToDo

---
+++ src/app/styles/themesUtils.tsx
@@ -0,0 +1,9 @@
+export const themeColors = {
+  buttonLight: "#38d38d",
+  buttonLightHover: "#16965d",
+  buttonDark: "#f86f6f",
+  buttonDarkHover: "#e13c3c",
+  letterDark: "#222222",
+  letterLight: "#ffffff",
+  backgroundLight: "#F2E6D8",
+};
---
+++ src/app/types/ToDoStructure.tsx
@@ -0,0 +1,7 @@
+export interface ToDoStructure {
+  id: number;
+  name: string;
+  done: boolean;
+}
+
+export type ToDos = ToDoStructure[];
